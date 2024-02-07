eaa36429c7f139aabd5b6178fb402ae457d28193
chinmaya-elred <chinmaya@elred.io>
2024-02-07 15:59:52 +0530

    fix safari favicon issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
