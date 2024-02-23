b8530b80344bb8328d52e39d1206155ad1f1d955
chinmaya-elred <chinmaya@elred.io>
2024-02-23 16:47:18 +0530

    add changes for headers

--- src/pages/shareProfile/index.js
+++ src/pages/shareProfile/index.js
@@ -54,38 +54,36 @@ function ShareProfile(props) {
 
 export async function getServerSideProps({ req, res, query }) {
   res.setHeader("Cache-Control", "no-store");
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', "application/json");
-  headers.append('X-ElRed-Test', Math.random() > 0.5 ? 'elRed-57c191ca14f63283': 'elRed-6d41c61445eb8f56')
+  // const headers = new Headers();
+  // headers.append('Content-Type', 'application/json');
+  // headers.append('Accept', "application/json");
+  // headers.append('X-ElRed-Test', Math.random() > 0.5 ? 'elRed-57c191ca14f63283': 'elRed-6d41c61445eb8f56')
 
-  const requestOptions = {
-    cache: "no-cache",
-    method: 'POST',
-    headers: headers
-  };
+  // const requestOptions = {
+  //   cache: "no-cache",
+  //   method: 'POST',
+  //   headers: headers
+  // };
   
   const userCode = query.userCode ?? "";
 
-  const request = new Request(`${baseURL}noSessionPreviewCardScreenshot?userCode=${userCode}`, requestOptions);
+  // const request = new Request(`${baseURL}noSessionPreviewCardScreenshot?userCode=${userCode}`, requestOptions);
 
 
-  // const response = await fetch(
-  //   `${baseURL}noSessionPreviewCardScreenshot?userCode=${userCode}`,
-  //   {
-  //     cache: "no-cache",
-  //     method: "POST",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       'X-ElRed-Test':  Math.random() > 0.5 ? 'elRed-57c191ca14f63283': 'elRed-6d41c61445eb8f56',
-  //     },
-  //   }
-  // );
+  const response = await fetch(
+    `${baseURL}noSessionPreviewCardScreenshot?userCode=${userCode}`,
+    {
+      cache: "no-cache",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        'X-ElRed-Test':  Math.random() > 0.5 ? 'elRed-57c191ca14f63283': 'elRed-6d41c61445eb8f56',
+      },
+    }
+  );
 
-  const response = await fetch(request);
-  console.log("request ===========>", request.headers, "request ===========>")
-  console.log("response ===========>", response.headers, "response ===========>")
+  // const response = await fetch(request);
 
   // console.log("request header after ===========>",req, req.headers,  'request header after')
   // console.log("=============>" , response.headers, 'response header after')
