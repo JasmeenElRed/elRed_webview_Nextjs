860dfb23b17deb97ad3e3bb7d600b1c90ef2362e
chinmaya-elred <chinmaya@elred.io>
2024-03-27 10:14:15 +0530

    add delete user account fucntionality in dev branch

---
+++ src/pages/deleteuseraccount/index.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+function DeleteUSerAccount() {
+  return (
+    <>
+     <Head>
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+
+        <meta property="og:title" content={"Elred Delete Account"} key="title" />
+        <meta property="og:description" content={"Elred Webview Delete Account"} />
+      </Head>
+
+      <div className="d-flex align-item-center justify-content-center height-100">
+        <iframe
+          allow="web-share"
+          src="https://el-red-deleteuseraccount-dev.vercel.app/"
+          className="iframe-cont"
+          title="Delete Elred User Account"
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default DeleteUSerAccount;
