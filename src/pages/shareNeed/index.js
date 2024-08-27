4c4bff8876734e807e5786e8ba8c9ebbec90b299
chinmaya-elred <chinmaya@elred.io>
2024-08-27 11:00:19 +0530

    checking meta data fr need

--- src/pages/shareNeed/index.js
+++ src/pages/shareNeed/index.js
@@ -14,7 +14,7 @@ function ShareNeed(props) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        {data?.needDescription && (<meta property="og:title" content={data.needDescription} key="title" />)} 
+        <meta property="og:title" content={data.needDescription || "webviewtestnext.elred.io"} key="title" />
         {data?.otherTags?.length && (
           <meta
             property="og:description"
