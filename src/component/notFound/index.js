1899d6eedb2e23c5be6e70b11f9f46edb6858ffe
chinmaya-elred <chinmaya@elred.io>
2024-09-06 15:58:11 +0530

    done changes for no internet screen

--- src/component/notFound/index.js
+++ src/component/notFound/index.js
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 function NotFound() {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center height-100">
+    <div className={styles.noInternetContainer}>
       <Image
         width={161}
         height={178}
-        src={"/noun-no-internet-access-4532233 1.png"}
+        src={"/noun-no-internet-access-4532233 1.svg"}
       />
       <div className={styles.oops_text}>Oops!</div>
       <div className={styles.error_text}>
