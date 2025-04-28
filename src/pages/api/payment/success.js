9be4fad98948fc0f0bdae3aa9e14236f64805c98
TashafMahmood <tashaf@elred.io>
2025-04-28 18:14:19 +0530

    changes for be api

--- src/pages/api/payment/success.js
+++ src/pages/api/payment/success.js
@@ -1,19 +1,6 @@
 // // pages/api/redirect.js
 
-// export default function handler(req, res) {
-//     res.redirect(307, '/payment/success');
-//   }
-  
-
-// pages/api/redirect.js
-
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-      // If it's a POST request, redirect to /payment/success
-      res.redirect(307, '/payment/success');
-    } else {
-      // If it's not a POST request, send 405 Method Not Allowed
-      res.status(405).json({ message: 'Method Not Allowed' });
-    }
+    res.redirect(307, '/payment/success');
   }
   
