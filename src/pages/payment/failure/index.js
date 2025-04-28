385b989d3cc26af8f88bc5e11c19831236758b26
TashafMahmood <tashaf@elred.io>
2025-04-28 13:30:26 +0530

    payu pages

---
+++ src/pages/payment/failure/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Failure = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center height-100">
+        Payment is Failed...
+    </div>
+  )
+}
+
+export default Failure
