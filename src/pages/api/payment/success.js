4d140465f3c320fa6ac61826f2513bad60046560
TashafMahmood <tashaf@elred.io>
2025-04-28 17:59:38 +0530

    changes

--- src/pages/api/payment/success.js
+++ src/pages/api/payment/success.js
@@ -1,6 +1,19 @@
+// // pages/api/redirect.js
+
+// export default function handler(req, res) {
+//     res.redirect(307, '/payment/success');
+//   }
+  
+
 // pages/api/redirect.js
 
 export default function handler(req, res) {
-    res.redirect(307, '/payment/success');
+    if (req.method === 'POST') {
+      // If it's a POST request, redirect to /payment/success
+      res.redirect(307, '/payment/success');
+    } else {
+      // If it's not a POST request, send 405 Method Not Allowed
+      res.status(405).json({ message: 'Method Not Allowed' });
+    }
   }
   
