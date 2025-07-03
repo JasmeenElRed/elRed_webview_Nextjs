3ab6e091495540c4ed884b6105d914b7bd1abcfe
TashafM <thecomputerpitch@gmail.com>
2025-07-03 17:10:18 +0530

    changes in api

--- src/pages/api/payment/failure.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-    res.redirect(301, '/payment/failure');
-  }
-
--- src/pages/api/payment/success.js
+++
@@ -1,6 +0,0 @@
-// // pages/api/redirect.js
-
-export default function handler(req, res) {
-    res.redirect(301, '/payment/success');
-}
-  
